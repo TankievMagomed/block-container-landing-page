bb901a09a04fb09f081a36824fec6bde2d61ef2a
Tankiev Magomed <tankievmi@bk.ru>
2026-05-16 20:47:58 +0300

    Upd

---
+++ recycling_of_garbage_landing_page/src/App.jsx
@@ -0,0 +1,16 @@
+import $ from 'react';
+import { Field } from './components/Field/Field';
+
+
+function App() {
+ 
+  return (
+    <div>
+      <Field>
+
+      </Field>
+    </div>
+  );
+}
+
+export default App;
---
+++ recycling_of_garbage_landing_page/src/sections/CompletedProjects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CompletedProjects = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
