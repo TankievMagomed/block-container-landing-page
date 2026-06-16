b1a2224d75b98439659424033f7c47c1a88a325a
Tankiev Magomed <tankievmi@bk.ru>
2026-06-16 12:33:42 +0300

    Работа над второй страницей

--- src/sections/CompletedProjects/CompletedProjects.jsx
+++ src/sections/CompletedProjects/CompletedProjects.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import $ from './CompletedProjects.module.css'
+import React from "react";
+import $ from "./CompletedProjects.module.css";
+import AlfaGroup from "../../assets/icon/Clients/AlfaGroup.webp";
+import AviaGroup from "../../assets/icon/Clients/AviaGroup.webp";
+import Mobil from "../../assets/icon/Clients/Mobil.webp";
+import Pic from "../../assets/icon/Clients/Pic.webp";
+import { ReactComponent as Samolet } from "../../assets/icon/Clients/samolet.svg";
+import Smart from "../../assets/icon/Clients/smart.webp";
 
 export const CompletedProjects = () => {
   return (
     <div className={$.completedProjects__section}>
-      
+      <h1 className={$.completedProjects__headerText}>
+        Нам доверили реализацию своих проектов
+        <br />
+        уже более 1250 компаний по всей России
+      </h1>
+      <span className={$.completedProjects__subHeaderText}>
+        Все проекты выполняются четко по требованию заказчика,
+        <br />
+        учитывая спецификацию, сроки и бюджет
+      </span>
+      <div className={$.completedProjects_logoSection}>
+        <img
+          src={Pic}
+          alt="Pic"
+        />
+        <img
+          src={AviaGroup}
+          alt="AviaGroup"
+        />
+        <Samolet />
+        <img
+          src={Smart}
+          alt="Smart"
+        />
+        <img
+          src={AlfaGroup}
+          alt="AlfaGroup"
+        />
+        <img
+          src={Mobil}
+          alt="Mobil"
+        />
+      </div>
     </div>
-  )
-}
-
+  );
+};
