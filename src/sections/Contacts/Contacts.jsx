9d16c7f48ceb716908ab787e7020ea285bcc423f
Magay Oleg <server90@mail.ru>
2026-08-01 18:03:18 +0300

    feature: Добавил яндекс карты на сайт

--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import $ from "./Contacts.module.css";
 import { ContactsInfo } from "components";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 export const Contacts = () => {
+  const defaultState = {
+    center: [55.751574, 37.573856],
+    zoom: 10,
+  };
+
   return (
     <div className={$.contacts__section}>
       <h1 className={$.contacts__headerText}>
         Центральный офис и производство находятся в <br /> г. Люберцы Московской
         области
       </h1>
-      <div className={$.contacts__map}></div>
+      <div className={$.contacts__map}>
+        <YMaps>
+          <Map
+            defaultState={defaultState}
+            style={{ width: 600, height: 400 }}
+          >
+            <Placemark geometry={[55.684758, 37.738521]} />
+          </Map>
+        </YMaps>
+      </div>
       <ContactsInfo />
     </div>
   );
